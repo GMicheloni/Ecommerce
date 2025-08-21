61f40126c28389ac8c326d6f0daabacb290d1afc
Gianfranco Micheloni <gmicheloni52@gmail.com>
2025-08-21 02:04:04 -0300

    docker compose funcionando

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('typeorm', () => ({
   type: 'postgres',
-  host: 'host.docker.internal',
+  host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432', 10),
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD,
